5c1b584748452d1bc8f80f2164fcae48bd02457e
Rishabh Kumar <work25860@gmail.com>
2021-08-12 15:33:06 +0530

    Adding Spring-Boot Project

--- spring-security/src/main/java/com/fis/springsecurity/WebSecurityConfig.java
+++ spring-security/src/main/java/com/fis/springsecurity/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
+				.antMatchers("/", "/home","/product").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
---
+++ spring-security/src/main/java/com/fis/springsecurity/product/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.fis.springsecurity.product;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface ProductRepository extends CrudRepository<Product, Integer>{
+
+}
